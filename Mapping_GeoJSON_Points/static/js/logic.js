f4323148aa35275acc3b8401aefbb221f251431b
boborodono <bowen.wilder@GMAIL.COM>
2022-01-26 21:03:11 -0600

    added files to Mapping_GeoJSON_Points branch

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,46 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Get data from cities.js
+let cityData = cities;
+
+let line = [
+    [33.9416, -118.4085],
+    [30.197614811682527, -97.66634217355856],
+    [43.678243566256974, -79.62519582879303],
+    [40.64753096623327, -73.79049148644886]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    lineweight: 4,
+    dashArray: "5, 5",
+    opacity: 0.5
+}).addTo(map);
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         color: "orange",
+//         fillOpacity: 0.2,
+//         radius: city.population / 200000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{styleId}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    styleId: "light-v10",
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
