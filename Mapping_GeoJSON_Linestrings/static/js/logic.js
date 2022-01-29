087278909e11adc16f359c9e22e81172ee2ca830
boborodono <bowen.wilder@GMAIL.COM>
2022-01-28 19:57:23 -0600

    Creating new branch Mapping_GeoJSON_Linestrings

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,44 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the satellite street view tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{styleId}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    styleId: "satellite-streets-v11",
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{styleId}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    styleId: "dark-v10",
+    accessToken: API_KEY
+});
+
+// Create a base layer map that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Pass pur map layers our layers control annd add the layers to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON UR
+let airportData = "https://raw.githubusercontent.com/boborodono/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/static/js/majorAirports.json";
+
+// Grabbing pur GeoJSON data.
+d3.json(airportData).then(function(data) {
+        console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
+});
